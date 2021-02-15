31ac046289b3f724b0c61253f4a74efd5b83c59f
mokhtartouiri <design.solutions.tn@gmail.com>
2021-02-15 14:17:15 +0000

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function that searches for a value in a sorted array of integers
+ * binary_search - function that searches for a value
  * @size: is the number of elements in array
  * @array:  is a pointer to the first element of the array
  * @value: is the value to search for Your function
